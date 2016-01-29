694b2384942c7dab38ec72216301cb5f43edf5c3
wataori <ropifuku@gmail.com>
2016-01-29 13:24:27 +0900

    Add Problem 5(one way)

---
+++ 005/005.rb
@@ -0,0 +1,11 @@
+module Solvee1
+  def solve
+    puts (0..Float::INFINITY).step(20).lazy.select{|n| detect(n.to_i, 20)}.first
+  end
+
+  def detect(num, divisor)
+    return true if divisor == 2 && num % divisor == 0
+    return false if num < 20
+    num % divisor == 0 ? detect(num / divisor, divisor - 1) : false
+  end
+end
