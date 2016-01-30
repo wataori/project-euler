e5a772ca04e3ad8cb08515a6f95eb79b4aa9e70c
wataori <ropifuku@gmail.com>
2016-01-30 17:46:58 +0900

    Update Problem 5 by ruby(later)

--- 005/005.rb
+++ 005/005.rb
@@ -1,11 +1,7 @@
-module Solve1
-  def solve
-    puts (0..Float::INFINITY).step(20).lazy.select{|n| detect(n.to_i, 20)}.first
-  end
-
-  def detect(num, divisor)
-    return true if divisor == 2 && num % divisor == 0
-    return false if num < 20
-    num % divisor == 0 ? detect(num, divisor - 1) : false
-  end
+def detect(num, divisor)
+  return true if divisor == 2 && num % divisor == 0
+  return false if num < 20
+  num % divisor == 0 ? detect(num, divisor - 1) : false
 end
+
+puts (0..Float::INFINITY).step(20).lazy.select{|n| detect(n.to_i, 20)}.first
