05c9547171c020f417879b303b909f2bcb0d9dbc
wataori <ropifuku@gmail.com>
2016-02-08 17:10:24 +0900

    Add 012.go

---
+++ 012/012.go
@@ -0,0 +1,31 @@
+package main
+import (
+  "fmt"
+  "math"
+)
+
+func main()  {
+  fmt.Printf("%v\n", solver(1))
+}
+
+func solver(n int) int {
+  triangul := n*(n+1)/2
+  count := count_divisors(2, triangul, 2)
+  if count >= 500 {
+    return triangul
+  } else {
+    return solver(n+1)
+  }
+}
+
+func count_divisors(n int, max int, count int) int {
+  if float64(n) >= math.Sqrt(float64(max)) {
+    return count
+  } else if max == 1 {
+    return 1
+  } else if max % n == 0 {
+    return count_divisors(n+1, max, count+2)
+  } else {
+    return count_divisors(n+1, max, count)
+  }
+}
